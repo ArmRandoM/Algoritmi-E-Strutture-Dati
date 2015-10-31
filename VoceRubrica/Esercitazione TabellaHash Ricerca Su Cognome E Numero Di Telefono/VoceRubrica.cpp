54bf010543709e70fb3877b83241fe721b3101a7
ArmandoPezz <armnd_6793@hotmail.it>
2015-10-31 03:54:09 +0100

    Modified By Armando

---
+++ VoceRubrica/Esercitazione TabellaHash Ricerca Su Cognome E Numero Di Telefono/VoceRubrica.cpp
@@ -0,0 +1,71 @@
+#include "VoceRubrica.h"
+
+//costruttore di default della classe
+VoceRubrica::VoceRubrica(){};
+
+//costruttore per copia della classe
+VoceRubrica::VoceRubrica( VoceRubrica const &vr)
+{
+  Nome = vr.Nome;
+  Cognome = vr.Cognome;
+  NumeroTelefonico = vr.NumeroTelefonico;
+}
+
+void VoceRubrica::setNome( string nome )
+{
+  Nome = nome;
+}
+
+void VoceRubrica::setCognome( string cognome )
+{
+  Cognome = cognome;
+}
+
+void VoceRubrica::setNumeroTelefonico( string numerotelefonico )
+{
+  NumeroTelefonico = numerotelefonico;
+}
+
+string VoceRubrica::getNome() const
+{
+  return Nome;
+}
+
+string VoceRubrica::getCognome() const
+{
+  return Cognome;
+}
+
+string VoceRubrica::getNumeroTelefonico() const
+{
+  return NumeroTelefonico;
+}
+
+//operatore di stampa ridefinito
+ostream &operator << ( ostream &out, const VoceRubrica& vr )
+{
+  cout << "===============================" << endl;
+  out << "Nome: " << vr.Nome << endl;
+  out << "Cognome: " << vr.Cognome << endl;
+  out << "Numero Telefonico: " << vr.NumeroTelefonico << endl;
+  cout << "===============================" << endl << endl;
+  return out;
+}
+
+//operatore di inserimento ridefinito
+istream &operator >> (istream &in, VoceRubrica& vr)
+{
+  cout << endl;
+  cout << "===============================" << endl;
+  cout << "Inserisci Voce Rubrica" << endl;
+  cout << "===============================" << endl;
+  cout << "Nome: ";
+  in >> vr.Nome;
+  cout << "Cognome: ";
+  in >> vr.Cognome;
+  cout << "Numero Telefonico: ";
+  in >> vr.NumeroTelefonico;
+  cout << "===============================" << endl;
+
+  return in;
+}
---
+++ VoceRubrica/Esercitazione TabellaHash Ricerca Su Cognome E Numero Di Telefono/main.cpp
@@ -0,0 +1,106 @@
+#include <iostream>
+#include <vector>
+#include "VoceRubrica.h"
+#include "TabellaHash.h"
+#include <string>
+#include <limits>
+
+using namespace std;
+
+//funzione che calcola un numero che identifica univocamente la stringa
+
+card string2hash( const string s )
+{
+	long int nMax = numeric_limits<long int>::max();
+	card C = 0;
+	for ( card i = 1; i <= s.size(); i++ )
+		C = (C + s[i]) % nMax;
+	return C;
+}
+
+// Chiave Hash di VoceRubrica calcolata sul Cognome dei singoli Contatti
+// definizione delle funzioni hash e equivalenti
+
+class HashVoceRubrica1 : public ChiaveHash<VoceRubrica>
+{
+  public:
+
+    card Hash( const VoceRubrica& vr )
+      {
+        return string2hash( vr.getCognome() );
+      }
+
+    bool Equivalenti(const VoceRubrica& vr1, const VoceRubrica& vr2)
+    {
+        return ( vr1.getCognome() == vr2.getCognome() );
+    }
+};
+
+class HashVoceRubrica2 : public ChiaveHash<VoceRubrica>
+{
+  public:
+
+    card Hash( const VoceRubrica& vr )
+      {
+        return string2hash( vr.getNumeroTelefonico() );
+      }
+
+    bool Equivalenti(const VoceRubrica& vr1, const VoceRubrica& vr2)
+    {
+        return ( vr1.getNumeroTelefonico() == vr2.getNumeroTelefonico() );
+    }
+};
+
+// main di esempio: cariamento di 10 Voci Rubrica ( contatti ) e ricerca per Cognome
+int main()
+{
+    HashVoceRubrica1 hCognome;
+		HashVoceRubrica2 hNumero;
+		VoceRubrica ContattoDaCercare;
+    string CognomeDaCercare;
+		string NumeroDaCercare;
+
+    //tabella hash di VoceRubrica con l'utilizzo della chiave hash sul Cognome
+    TabellaHash<VoceRubrica> Rubrica1( hCognome, 100);
+		TabellaHash<VoceRubrica> Rubrica2( hNumero, 100);
+		VoceRubrica vr;
+
+    for(int i = 1; i<= 4; i++)
+      {
+        cin >> vr; //operatore >> ridefinito in VoceRubrica
+        Rubrica1.Ins(vr); //inserimento nella tabella hash Rubrica con hashing per cognome
+				Rubrica2.Ins(vr); //inserimento nella tabella hash Rubrica con hashing per numero di telefono
+      }
+
+    cout << endl;
+		cout << "===============================" << endl;
+    cout << "Cognome del contatto da cercare:" << endl;
+    cin >> CognomeDaCercare;
+		cout << "Numero di telefono del contatto da cercare:" << endl;
+		cin >> NumeroDaCercare;
+		cout << "===============================" << endl;
+    ContattoDaCercare.setCognome( CognomeDaCercare );
+		ContattoDaCercare.setNumeroTelefonico( NumeroDaCercare );
+		
+    if (Rubrica1.Cerca(ContattoDaCercare)) //ricerca e assegnamento del contatto trovato nella Rubrica
+      	{
+					cout << endl << endl;
+					cout << "===============================" << endl;
+					cout << "Specifiche Contatto Cercato" << endl;
+					cout << ContattoDaCercare;
+				}
+    else if(Rubrica2.Cerca( ContattoDaCercare))
+				{
+					cout << endl << endl;
+					cout << "===============================" << endl;
+					cout << "Specifiche Contatto Cercato" << endl;
+					cout << ContattoDaCercare;
+
+				}
+			else
+    		cout << endl << "Non trovato!!!" << endl << endl;
+
+		//Rubrica2.Stampa();
+
+    return 0;
+}
