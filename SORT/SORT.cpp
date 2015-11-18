9665470e6ce56792b5c2354e17f3dcbd2d1ec812
ArmandoPezz <armnd_6793@hotmail.it>
2015-11-18 16:47:50 +0100

    Modified By Armando

--- SORT/SORT.cpp
+++ SORT/SORT.cpp
@@ -1,32 +1,31 @@
-#include <iostream>
+#include <stdio.h>
 
 int main()
 {
-
   int numero;
   int ElencoNumeri[1000] = {0};
   int max = 0;
 
-  std::cin >> numero;
+  scanf("%d", &numero);
 
   while( numero != -1 )
     {
       ElencoNumeri[numero] += 1;
-      std::cin >>numero;
+      scanf("%d", &numero);
       if( numero > max )
         max = numero+1;
     }
 
-  for( int i = 0; i < max; i++ )
+  for( int i = 0; i < max; ++i )
     {
       if( ElencoNumeri[i] != 0 )
         {
           if( ElencoNumeri[i] == 1 )
-            std::cout << i << std::endl;
+            printf("%d\n", i );
           else
             {
-              for( int j = 0; j < ElencoNumeri[i]; j++)
-                std::cout << i << std::endl;
+              for( int j = 0; j < ElencoNumeri[i]; ++j)
+                printf("%d\n", i );
             }
         }
     }
